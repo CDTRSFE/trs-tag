e95f3b50cf51f514f96259ef53679a4c07cf0eb5
何建博 <hjb2722404@163.com>
2021-02-05 16:50:46 +0800

    更新若干

--- bin/gt-last.js
+++
@@ -1,4 +0,0 @@
-var shell = require('shelljs');
-
-var r = shell.exec(`git tag -l "xjzmy-prod-v*" --sort=-v:refname | head -n 1`, { silent: true });
-
--- bin/gt.js
+++ bin/gt.js
@@ -3,10 +3,10 @@
 const program = require('commander');
 
 program
-    .usage('<command> --[env]')
+    .usage('<command>')
     .version(require('../package.json').version)
     .command('ver', '更新版本号')
     .command('tag', '创建tag、推送tag')
     .command('init', '创建配置文件')
-    .command('last', '查看最后一个tag')
+    .command('githelp', 'git命令速查')
     .parse(process.argv)
